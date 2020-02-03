2d38e6cb8d6d2f9fae8b4b73bdefdb5816dbfa68
gokuldas <gokuldas@192.168.100.53>
2020-02-03 23:58:16 +1100

    new project skeleton

---
+++ practiceauto/src/test/java/goksselenium/practice2Test.java
@@ -0,0 +1,16 @@
+package goksselenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class practice2Test {
+	
+	
+	@Test
+	public void logintestnew()
+	{
+		
+		System.out.println("google is new");
+	}	
+
+}
---
+++ practiceauto/src/test/java/goksselenium/practiceTest.java
@@ -0,0 +1,16 @@
+package goksselenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class practiceTest {
+	
+
+@Test
+public void logintest()
+{
+	
+	System.out.println("google");
+}
+
+}
